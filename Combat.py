becf484d492ea6c0c6e11d323e6eecb48bd517f5
lucas-bendia <lucas.bendia@laplateforme.io>
2024-01-18 16:09:28 +0100

    mise a jour

--- Combat.py
+++ Combat.py
@@ -1,21 +1,38 @@
 import random
 
-
 class Pokemon:
-    def __init__(self, name, type1, type2, hp, atk, df, spatk, spdf, spd, move1, move2, move3, move4):
-        self.name = name
+    def __init__(self, nom, type1, type2, pv, atk, df, atkspe, dfspe, vit, attaque1, attaque2, attaque3, attaque4, combats_gagnes=0):
+        self.nom = nom
         self.type1 = type1
         self.type2 = type2
-        self.hp = hp
+        self.pv = pv
         self.atk = atk
         self.df = df
-        self.spatk = spatk
-        self.spdf = spdf
-        self.spd = spd
-        self.move1 = move1
-        self.move2 = move2
-        self.move3 = move3
-        self.move4 = move4
+        self.atkspe = atkspe
+        self.dfspe = dfspe
+        self.vit = vit
+        self.attaque = [attaque1, attaque2, attaque3, attaque4]
+        self.combats_gagnes = combats_gagnes
+        self.attaques_potentielles = []
+        self.evolutions = {"Salamèche": "Reptincel", "Reptincel": "Dracaufeu", "Carapuce": "Carabaffe", "Carabaffe": "Tortank", "Bulbizarre": "Herbizarre", "Herbizarre": "Florizarre"}
+
+    def gagner_combat(self):
+        self.combats_gagnes += 1
+        if self.combats_gagnes % 4 == 0:  # Si le Pokémon a gagné 4 combats
+            self.apprendre_attaque()  # Le Pokémon devrait apprendre une nouvelle attaque
+            self.evoluer()  # Le Pokémon devrait évoluer
+
+    def apprendre_attaque(self):
+        if self.attaques_potentielles:  # S'il y a des attaques potentielles à apprendre
+            self.attaque.append(self.attaques_potentielles.pop(0))  # Apprendre la première attaque potentielle
+
+    def evoluer(self):
+        if self.nom in self.evolutions:
+            self.nom = self.evolutions[self.nom]
+            print(f"Félicitations! Votre {self.nom} a évolué!")
+        else:
+            print(f"{self.nom} a atteint son évolution finale.")
+            self.evolutions.pop(self.nom, None)  # Retirer le Pokémon du dictionnaire des évolutions
 
 
 class Move:
@@ -49,20 +66,56 @@ Tempête_Florale = Move("Tempête_Florale", "Physique", "plante", 90)
 Lance_Soleil = Move("Lance_Soleil", "Special", "plante", 120)
 Griffe = Move("Griffe", "physique", "Normal", 40)
 Hydrocanon = Move("Hydrocanon", "special", "Eau", 120)
+Éclair = Move("Éclair", "special", "Electrik", 40)
+Vive_Attaque = Move("Vive_Attaque", "physique", "Normal", 40)
+Frotte_Frimousse = Move("Frotte_Frimousse", "physique", "Electrik", 20)
+
 
 
 #Pokemon stats + attaque
 
-Pok1 = Pokemon("Salamèche", "Feu", None, 39, 56, 47, 60, 50, 65, Flammeche, Charge, Damoclès,Crocs_Feu)
-Pok2 = Pokemon("Reptincel", "Feu", None, 58, 64, 58, 80, 65, 80, Flammeche, Charge, Damoclès,Crocs_Feu)
-Pok3 = Pokemon("Dracaufeu", "Feu", "Vol", 185, 149, 143, 177, 150, 167, Lanceflamme, Lame_dair, Boutefeu, Tranche)
-Pok4 = Pokemon("Carapuce", "Eau", None, 44, 48, 65, 50, 64, 43, Pistolet_a_O, Morsure, Charge,Damoclès)
-Pok5 = Pokemon("Carabaffe", "Eau", None, 59, 63, 80, 65, 80, 58, Pistolet_a_O, Morsure, Charge,Damoclès)
-Pok6 = Pokemon("Tortank", "Eau", None, 186, 148, 167, 150, 172, 143, Vibraqua, Hydro_queue, Morsure, Tour_rapide)
-Pok7 = Pokemon("Bulbizarre", "Plante", "Poison", 45, 49, 49, 65, 65, 45, Faux_Coup, Fouet_Lianes, Damoclès, Charge)
-Pok8 = Pokemon("Herbizarre", "Plante", "Poison", 60, 62, 63, 80, 80, 60, Faux_Coup, Fouet_Lianes, Damoclès, Charge)
-Pok9 = Pokemon("Florizarre", "Plante", "Poison", 187, 147, 148, 167, 167, 145, Canon_graine, Bombe_Beurk, Damoclès,
+Pok1 = Pokemon("Salamèche", "Feu", None, 39, 56, 47, 60, 50, 65, Flammeche, Charge, None,None)
+Pok2 = Pokemon("Reptincel", "Feu", None, 58, 64, 58, 80, 65, 80, Flammeche, Charge, None,Crocs_Feu)
+Pok3 = Pokemon("Dracaufeu", "Feu", "Vol", 185, 149, 143, 177, 150, 167, Lanceflamme, None, Boutefeu, Tranche)
+Pok4 = Pokemon("Carapuce", "Eau", None, 44, 48, 65, 50, 64, 43, Pistolet_a_O, None, Charge,None)
+Pok5 = Pokemon("Carabaffe", "Eau", None, 59, 63, 80, 65, 80, 58, Pistolet_a_O, Morsure, Charge,None)
+Pok6 = Pokemon("Tortank", "Eau", None, 186, 148, 167, 150, 172, 143, Vibraqua, Tour_rapide, Morsure, None)
+Pok7 = Pokemon("Bulbizarre", "Plante", "Poison", 45, 49, 49, 65, 65, 45, None, Fouet_Lianes, None, Charge)
+Pok8 = Pokemon("Herbizarre", "Plante", "Poison", 60, 62, 63, 80, 80, 60, Faux_Coup, Fouet_Lianes, None, Charge)
+Pok9 = Pokemon("Florizarre", "Plante", "Poison", 187, 147, 148, 167, 167, 145, Bombe_Beurk, None, Damoclès,
                Coupe_feuille)
+Pok10 = Pokemon("Pikachu", "Electrik", None, 35, 55, 40, 50, 50, 90, Éclair, Vive_Attaque, Frotte_Frimousse, Charge)
+
+# Attaques potentielles pour chaque Pokemon
+attaques_potentielles_Pok1 = [Crocs_Feu]
+attaques_potentielles_Pok2 = [Damoclès]
+attaques_potentielles_Pok3 = [Lame_dair]
+attaques_potentielles_Pok4 = [Morsure]
+attaques_potentielles_Pok5 = [Damoclès]
+attaques_potentielles_Pok6 = [Hydro_queue]
+attaques_potentielles_Pok7 = [Faux_Coup]
+attaques_potentielles_Pok8 = [Damoclès]
+attaques_potentielles_Pok9 = [Canon_graine]
+
+
+# Assignation des attaques potentielles à chaque Pokemon
+Pok1.attaques_potentielles = attaques_potentielles_Pok1
+Pok2.attaques_potentielles = attaques_potentielles_Pok2
+Pok3.attaques_potentielles = attaques_potentielles_Pok3
+Pok4.attaques_potentielles = attaques_potentielles_Pok4
+Pok5.attaques_potentielles = attaques_potentielles_Pok5
+Pok6.attaques_potentielles = attaques_potentielles_Pok6
+Pok7.attaques_potentielles = attaques_potentielles_Pok7
+Pok8.attaques_potentielles = attaques_potentielles_Pok8
+Pok9.attaques_potentielles = attaques_potentielles_Pok9
+
+# Liste des Pokemons
+pokemons = [Pok1, Pok2, Pok3, Pok4, Pok5, Pok6, Pok7, Pok8, Pok9]
+
+# Chaque Pokemon gagne 4 combats
+for pokemon in pokemons:
+    for _ in range(4):
+        pokemon.gagner_combat()  # Après ces combats, chaque Pokemon devrait apprendre une nouvelle attaque
 
 
 def PokAttack(pok1, pok2, move):
@@ -107,6 +160,10 @@ def PokAttack(pok1, pok2, move):
     if move.typ == "Eau" and pok2.type1 == "Feu":
         damage *= 1.8
     if move.typ == "Vol" and pok2.type1 == "Plante":
+        damage *= 1.8
+    if move.typ == "Electrik" and pok2.type1 == "Vol":
+        damage *= 1.8
+    if move.typ == "Electrik" and pok2.type1 == "Eau":
         damage *= 1.8
 
     damage = round(damage, 0)
